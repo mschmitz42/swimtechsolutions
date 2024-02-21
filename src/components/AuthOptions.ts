a8cd0285bc9618d7d131162ddc8fc77bd84227e3
mschmitz42 <119008661+mschmitz42@users.noreply.github.com>
2024-02-20 17:46:50 -0800

    signin with google

--- src/components/AuthOptions.ts
+++ src/components/AuthOptions.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import { getUserByEmail } from "@/db/users";
 import { compare } from "bcrypt";
 
@@ -50,6 +51,10 @@ export const AuthOptions: NextAuthOptions = {
         GithubProvider({
             clientId: process.env.GITHUB_ID ?? "",
             clientSecret: process.env.GITHUB_SECRET ?? "",
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID ?? "",
+            clientSecret: process.env.GOOGLE_SECRET ?? "",
         }),
     ],
 }
