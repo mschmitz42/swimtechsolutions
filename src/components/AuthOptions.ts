010b03757a692bf6867a0aa3879d825b976b5732
mschmitz42 <119008661+mschmitz42@users.noreply.github.com>
2024-02-20 18:38:11 -0800

    fixed google in authOptions

--- src/components/AuthOptions.ts
+++ src/components/AuthOptions.ts
@@ -53,8 +53,8 @@ export const AuthOptions: NextAuthOptions = {
             clientSecret: process.env.GITHUB_SECRET ?? "",
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID ?? "",
-            clientSecret: process.env.GOOGLE_SECRET ?? "",
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         }),
     ],
 }
