a452f326882020ac574f501d027df8ae07b9538a
mschmitz42 <119008661+mschmitz42@users.noreply.github.com>
2024-02-28 19:37:07 -0800

    video updates

--- src/app/ui/components/VideoList.tsx
+++ src/app/ui/components/VideoList.tsx
@@ -1,6 +1,7 @@
 import { getUserVideos } from "@/db/videos"
-import Link from "next/link"
-import Image from "next/image"
+// import Link from "next/link"
+// import Image from "next/image"
+// import DOMPurify from "isomorphic-dompurify"
 
 
 export default async function VideoList ( { userId }: {userId: string}  ) {
@@ -9,14 +10,15 @@ export default async function VideoList ( { userId }: {userId: string}  ) {
 
     return (
         <>
-        <div className="card-grid">
+        <div className="card-grid video-card-grid">
             {videos.map(video => (
                 <div key={video.id} className="card">
-                <div className="card-header">
+                <div className="card-header video-card-header">
                     {video.title}
                </div>
-                <div className="card-body">
-                    <iframe width="560" height="315" src={video.embedCode} title={video.title}></iframe>
+                <div className="card-body video-card-body">
+                {/* <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(video.embedCode)}}></div> */}
+                <div dangerouslySetInnerHTML={{__html: video.embedCode}}></div>
                 </div>
                 <div className="card-footer">
                     {/* <Link className="btn" href={video.url}>
